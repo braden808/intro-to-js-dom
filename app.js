ede3d5a72998ebecbe189576b3196e65c5731d7f
Braden Amorozo <braden808@gmail.com>
2017-03-13 08:45:08 -1000

    update

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ console.log(idElement);
 //console.log(document.getElementsByTagName("div"));
 var tagElements = document.getElementsByTagName("div");
 console.log(tagElements)
+var classElements = document.getElementsByClassName("use-class-method")
+
+//changing html elements
+
+console.log(idElement.innerHTML);
+idElement.innerHTML = "I have been changed";
+
+console.log(classElements[1]);
+classElements[1]; = "I changed by class"

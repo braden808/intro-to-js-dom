6d8be6c0ffe4fd74e80743da85616a432042fdc9
Braden Amorozo <braden808@gmail.com>
2017-03-13 08:34:02 -1000

    update

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Sanity Check")
+console.log("Sanity Check");
+//console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method")
+console.log(idElement);
+//console.log(document.getElementsByTagName("div"));
+var tagElements = document.getElementsByTagName("div");
+console.log(tagElements)
